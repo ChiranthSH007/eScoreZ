08827bbcf3811c127aa9d52c82734a6014fc6180
Sujay PV <sujaylords100@gmail.com>
2021-06-18 11:55:19 +0530

    added app bar

---
+++ lib/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: new Column(
+      children: [],
+    ));
+  }
+}
