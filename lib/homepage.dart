1b3445db89ec06e5db603bb9cb62643eb0242bdc
ChiranthSh007 <chiranthsh007@gmail.com>
2021-06-18 12:25:28 +0530

    test 1

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:esportzzz/HomePageBody.dart';
+import 'package:esportzzz/appbar.dart';
 import 'package:flutter/material.dart';
 
 class homepage extends StatelessWidget {
@@ -9,6 +10,7 @@ class homepage extends StatelessWidget {
     return new Scaffold(
         body: new Column(
       children: <Widget>[
+        new GradientAppBar("eSportzzz"),
         new HomePageBody(),
       ],
     ));
