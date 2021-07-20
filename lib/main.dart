d4552f0f36414bfa9ddec50ea00da357713d3df0
ChiranthSh007 <chiranthsh007@gmail.com>
2021-07-20 12:25:20 +0530

    hgjhgad

--- lib/main.dart
+++ lib/main.dart
@@ -8,62 +8,12 @@ import 'package:esportzzz/newshomepage.dart';
 import 'package:esportzzz/splashscreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _initialized = false;
-  bool _error = false;
-  void initializeFlutterFire() async {
-    try {
-      // Wait for Firebase to initialize and set `_initialized` state to true
-      await Firebase.initializeApp();
-      setState(() {
-        _initialized = true;
-      });
-    } catch (e) {
-      // Set `_error` state to true if Firebase initialization fails
-      setState(() {
-        _error = true;
-      });
-    }
-  }
-
-  void initState() {
-    super.initState();
-    initializeFlutterFire();
-  }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    if (_error) {
-      print("ERROR IS      ");
-      print(_error);
-    }
-
-    // Show a loader until FlutterFire is initialized
-    if (!_initialized) {
-      return CircularProgressIndicator();
-    }
-    return MaterialApp(
-      title: 'eScoreZ',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: new Login_Select(),
-    );
-  }
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  runApp(Splash_Screen());
 }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:esportzzz/login_select_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:video_player/video_player.dart';
 
 class splash_screen extends StatelessWidget {
   const splash_screen({Key? key}) : super(key: key);
@@ -10,6 +15,89 @@ class splash_screen extends StatelessWidget {
       body: Container(
         child: Center(
           child: Text("Welcome To eSportzzz mobile App"),
+        ),
+      ),
+    );
+  }
+}
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({Key? key}) : super(key: key);
+
+  @override
+  _Splash_ScreenState createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  bool _initialized = false;
+  late VideoPlayerController _controller;
+  bool _error = false;
+  void initializeFlutterFire() async {
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("lib/assets/eScoreZLogo.mp4");
+    _controller.initialize().then((_) {
+      _controller.setLooping(true);
+      Timer(Duration(seconds: 4), () {
+        setState(() {
+          _controller.play();
+        });
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Login_Select()));
+      });
+    });
+    initializeFlutterFire();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    if (_error) {
+      print("ERROR IS      ");
+      print(_error);
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      return CircularProgressIndicator();
+    }
+    return MaterialApp(
+      title: 'eScoreZ',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: new Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            SizedBox(
+              width: _controller.value.size.width,
+              height: _controller.value.size.height,
+              child: VideoPlayer(_controller),
+            )
+          ],
         ),
       ),
     );
